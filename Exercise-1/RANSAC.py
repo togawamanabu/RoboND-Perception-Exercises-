ddc06b548f35d212a66780f08e15350bdfea44b6
togawa manabu <togawamanabu@gmail.com>
2017-07-21 15:33:47 -0700

    feat: point clound voxel grid downsampling and pass through filtering

--- Exercise-1/RANSAC.py
+++ Exercise-1/RANSAC.py
@@ -5,10 +5,28 @@ import pcl
 cloud = pcl.load_XYZRGB('tabletop.pcd')
 
 # Voxel Grid filter
+vox = cloud.make_voxel_grid_filter()
 
+LEAF_SIZE = 0.01
+
+vox.set_leaf_size(LEAF_SIZE, LEAF_SIZE, LEAF_SIZE)
+
+cloud_filterd = vox.filter()
+filename = 'voxel_downsampled.pcd'
+pcl.save(cloud_filterd, filename)
 
 # PassThrough filter
+passthrough = cloud_filterd.make_passthrough_filter()
 
+filter_axis = 'z'
+passthrough.set_filter_field_name(filter_axis)
+axis_min = 0.6
+axis_max = 1.1
+passthrough.set_filter_limits (axis_min, axis_max)
+
+cloud_filtered = passthrough.filter()
+filename = 'pass_through_filtered.pcd'
+pcl.save(cloud_filtered, filename)
 
 # RANSAC plane segmentation
 
